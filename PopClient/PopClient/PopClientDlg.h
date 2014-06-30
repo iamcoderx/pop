f0b7d1b8fc60489d26ad8439dbcbfa37068b06e4
spade5 <15871747640@qq.com>
2014-06-30 09:20:11 +0800

    新建项目

---
+++ PopClient/PopClient/PopClientDlg.h
@@ -0,0 +1,32 @@
+
+// PopClientDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CPopClientDlg 对话框
+class CPopClientDlg : public CDialogEx
+{
+// 构造
+public:
+	CPopClientDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_POPCLIENT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
